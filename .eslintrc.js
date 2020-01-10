1e894a893d336a812dd8d6248e17411916c534a5
kenzochen <chenjiang@xywifi.com>
2020-01-10 14:39:53 +0800

    初始化eslint和prettier配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
+    es6: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semi: ['error', 'always'],
+    camelcase: 'off',
+    eqeqeq: 'off',
+    'no-trailing-spaces': [
+      'warn',
+      { skipBlankLines: true, ignoreComments: true }
+    ],
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-unused-vars': ['error', { caughtErrors: 'none', args: 'after-used' }],
+    'object-curly-spacing': ['error', 'always'],
+    'max-len': 'off'
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
+    ecmaVersion: 6
   }
 };
